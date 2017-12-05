835ac0c5b05d7e57358408b8994768fe9169eaf5
k-jewon <kjw2051182@gmail.com>
2017-12-05 21:17:41 +0900

    UserRestController클래스 추가, login post REST API추가

--- UML01Board/sql/dml.sql
+++ UML01Board/sql/dml.sql
@@ -10,9 +10,9 @@ VALUES ('admin@koitt.com',
 '관리자', NULL);
 
 INSERT INTO user_info(email, password, name, avatar)
-VALUES ('hoon@koitt.com', 
+VALUES ('kjw@koitt.com', 
 '$2a$10$b.Epv/t3IKT8v7oh4Byx8.1G7WszQRm42a6Nvbpne0o4BsA/4sUOi',
-'정상훈', NULL);
+'김제원', NULL);
 
 INSERT INTO user_info_type(user_info_email, user_type_id)
 VALUES ('admin@koitt.com', 1);
@@ -21,7 +21,7 @@ INSERT INTO user_info_type(user_info_email, user_type_id)
 VALUES ('admin@koitt.com', 2);
 
 INSERT INTO user_info_type(user_info_email, user_type_id)
-VALUES ('hoon@koitt.com', 2);
+VALUES ('kjw@koitt.com', 2);
 
 INSERT INTO board VALUES(board_seq.NEXTVAL, 'TITLE1', 'CONTENT-1', 'admin@koitt.com', SYSDATE, NULL);
 INSERT INTO board VALUES(board_seq.NEXTVAL, 'TITLE2', 'CONTENT-2', 'admin@koitt.com', SYSDATE, NULL);
